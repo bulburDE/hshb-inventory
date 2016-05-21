97236701906e8a565b78d885610780d4043d9e30
Thomas Helmke <thomas.helmke@gmx.de>
2016-05-21 23:03:01 +0200

    done: basic funcionality to build a inventory db todo: connect to gui

--- InventoryItemList.py
+++ InventoryItemList.py
@@ -4,23 +4,71 @@ reload(sys)
 sys.setdefaultencoding('utf-8')
 import yourls
 import sqlite3
+import getpass
+import dokuwiki
+import re
 
-baseurl = 'http://hshb.de/yourls-api.php'
-with open('yourls-signature.txt','r') as s:
-    sig = s.read().strip()
 
 class InventoryItemList:
-    def __init__(self, baseurl, signature, dbname):
+    def __init__(self, yourlsurl, yourlssig, wikiurl, wikiuser, wikipw, dbname):
         self.prefix = "g"
-        self.yourls = yourls.YOURLSClient(baseurl, signature=signature)
+        self.h1regex = re.compile("={6}\s(.+?)\s={6}")
+        self.yourls = yourls.YOURLSClient(yourlsurl, signature=yourlssig)
         self.db = sqlite3.connect(dbname)
         self.dbcursor = self.db.cursor()
         self.dbcursor.execute("""CREATE TABLE IF NOT EXISTS Inventory 
-                              (Number INT NOT NULL, Title TEXT NOT NULL, ShortUrl TEXT NOT NULL, WikiUrl TEXT NOT NULL)""")
+                              (Number INT UNIQUE NOT NULL, Title TEXT NOT NULL)""")
         self.db.commit()
 
+        self.wiki = dokuwiki.DokuWiki(wikiurl, wikiuser, wikipw)
+
     def Add(self, number):
-        pass
+        shorturl = self.prefix + format(number, "04")
+        print shorturl
+        try:
+            wikiurl = self.yourls.expand(shorturl)
+            print wikiurl
+        except:
+            print "shorturl not found"
+            return
+        try:
+            pagename = self.NamespaceFromUrl(wikiurl)
+            print pagename
+            if self.wiki.pages.info(pagename):
+                page = self.wiki.pages.get(pagename)
+                print page[:20]
+            else:
+                raise
+        except:
+            print "wiki url not well formed or page does not exist"
+            return
+        try:
+            title = self.h1regex.match(page).group(1)
+            print title
+        except:
+            print "page title not found. wiki page not formated correctly."
+            return
+        try:    
+            self.dbcursor.execute("INSERT INTO Inventory VALUES (?, ?)", (number, title))
+            self.db.commit()
+        except sqlite3.IntegrityError:
+            print "adding failed, most likely entry already exists"
+
+    def NamespaceFromUrl(self, url):
+        return ':'.join(url.split('/')[3:])
+
 
 if __name__ == "__main__":
-    inv = InventoryItemList(baseurl, sig, "test.db")
+    yourlsurl = 'http://hshb.de/yourls-api.php'
+    with open('yourls-signature.txt','r') as s:
+        sig = s.read().strip()
+
+    wikiurl = 'https://wiki.hackerspace-bremen.de'
+    user = 'heth'
+    pw = getpass.getpass('Passwort: ')
+
+    inv = InventoryItemList(yourlsurl, sig, wikiurl, user, pw, "test.db")
+    for i in range(1, 110):
+        inv.Add(i)
+    inv.Add(1234)
+    inv.Add(1235)
