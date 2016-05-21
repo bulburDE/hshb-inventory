0380486cbfe7213d37dda3f9ea6c7efaa6cb4e99
Thomas Helmke <thomas.helmke@gmx.de>
2016-05-21 19:12:23 +0200

    Basic GUI completed

--- wxgui.py
+++ wxgui.py
@@ -9,7 +9,7 @@ class InvFrame(wx.Frame):
 
         self.OverallSizer = wx.BoxSizer(wx.VERTICAL)
 
-        self.NewEntryBox = wx.StaticBox(self, -1, _('New Entry'))
+        self.NewEntryBox = wx.StaticBox(self, wx.ID_ANY, _("New Entry"))
         self.NewEntryBoxSizer = wx.StaticBoxSizer(self.NewEntryBox, wx.VERTICAL)
 
         self.NewEntrySizer = wx.GridSizer(2, 2, 5, 5)
@@ -24,8 +24,36 @@ class InvFrame(wx.Frame):
         self.NewEntrySizer.Add(self.TitleStat, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL)
         self.NewEntrySizer.Add(self.TitleText, 0, wx.ALL|wx.ALIGN_CENTER_VERTICAL)
 
+        self.NewButton = wx.Button(self, wx.ID_ANY, _("Create new entry"))
+
         self.NewEntryBoxSizer.Add(self.NewEntrySizer)
-        self.OverallSizer.Add(self.NewEntryBoxSizer)
+        self.NewEntryBoxSizer.Add(self.NewButton, 0, wx.ALL|wx.EXPAND|wx.ALIGN_CENTER_VERTICAL)
+
+        self.OldEntryBox = wx.StaticBox(self, wx.ID_ANY, _("Existing Entries"))
+        self.OldEntryBoxSizer = wx.StaticBoxSizer(self.OldEntryBox, wx.HORIZONTAL)
+
+        self.OldEntrySizer = wx.BoxSizer(wx.HORIZONTAL)
+        self.EntryList = wx.ListCtrl(self, style=wx.LC_REPORT)
+        self.EntryList.InsertColumn(0,_("Number"))
+        self.EntryList.InsertColumn(1,_("Title"))
+
+        self.ListButtonSizer = wx.BoxSizer(wx.VERTICAL)
+
+        self.CreateLabelButton = wx.Button(self, wx.ID_ANY, _("Create Label"))
+        self.IncrementalUpdateButton = wx.Button(self, wx.ID_ANY, _("Incremental Update"))
+        self.CompleteUpdateButton = wx.Button(self, wx.ID_ANY, _("Complete Update"))
+
+        self.ListButtonSizer.Add(self.CreateLabelButton, 0, wx.ALL|wx.EXPAND)
+        self.ListButtonSizer.Add(self.IncrementalUpdateButton, 0, wx.ALL|wx.EXPAND)
+        self.ListButtonSizer.Add(self.CompleteUpdateButton, 0, wx.ALL|wx.EXPAND)
+
+        self.OldEntrySizer.Add(self.EntryList, 1, wx.ALL|wx.EXPAND)
+
+        self.OldEntryBoxSizer.Add(self.OldEntrySizer, 1, wx.ALL|wx.EXPAND)
+        self.OldEntryBoxSizer.Add(self.ListButtonSizer, 0, wx.ALL|wx.EXPAND)
+
+        self.OverallSizer.Add(self.NewEntryBoxSizer, 0, wx.ALL|wx.EXPAND)
+        self.OverallSizer.Add(self.OldEntryBoxSizer, 1, wx.ALL|wx.EXPAND)
 
         self.SetSizer(self.OverallSizer)
         self.Show(True)
